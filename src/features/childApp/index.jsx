7742525dd5cbea9fe3dbacd319fc6e580f3a5119
Nikita Slupko <nikita.slupko@carrier.com>
2022-08-20 15:56:06 +0400

    add geo sending

--- src/features/childApp/index.jsx
+++ src/features/childApp/index.jsx
@@ -1,36 +1,47 @@
 import Web3 from 'web3/dist/web3.min.js';
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 import {CONTACT_ABI, CONTACT_ADDRESS} from "../../contracts/const";
 import {useParents} from './assignedParents/hooks/useParents';
 
 export const ChildPage = () => {
     const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-    const { parents = [], loadParents, isLoading, setParent } = useParents();
+    const { isLoading, setParent } = useParents();
     const [parentId, setParentId] = useState();
     const [account, setAccount] = useState();
+    const [parents, setParents] = useState([]);
     const [contractList, setContractList] = useState();
+
+    const storeGeo = useRef(null);
+
+    const processRegister = async (accounts) => {
+        const [childAccount] = accounts;
+        console.log('ACC', childAccount);
+        setAccount(childAccount);
+        const contract = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
+        setContractList(contract.methods);
+        console.log('ACCOUNT', childAccount)
+        const isRegistered = await contract.methods.isRegistered().call({from: childAccount});
+        console.log("[IS REGISTERED]", isRegistered);
+        if (!isRegistered) {
+            const device = await contract.methods.addDevice().send({from: childAccount});
+            console.log("[NEW DEVICE]", device);
+        }
+        const spectators = await contract.methods.getSpectator().call({from: childAccount});
+        setParents([spectators]);
+
+        storeGeo.current = async () => {
+            const long = String(44.762258 + Math.random() / 100);
+            const lat = String(41.7149921 + Math.random() / 100);
+            await contract.methods.storeGeo(+new Date(), long, lat).send({from: childAccount});
+            console.log('[GEO SENT]', [long, lat])
+        }
+    }
 
     useEffect(() => {
         const load = async () => {
-            await loadParents();
-            const [childAccount] = await web3.eth.requestAccounts();
-            setAccount(childAccount);
-            const contract = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
-            setContractList(contract.methods);
-            // const isRegistered = await contract.methods.isRegistered().call();
-            // console.log("[IS REGISTERED 1]", isRegistered);
-            // const device = await contract.methods.addDevice().send({from: account});
-            // console.log("NEW DEVICE", device);
-            // const isRegistered2 = await contract.methods.isRegistered().call();
-            // console.log("[IS REGISTERED 2]", isRegistered2);
-            // const spectator = await contract.methods.getSpectator().call();
-            // console.log('[SPEC]', spectator)
-            // const counter = await contractList.methods.addDevice().call();
-            // console.log('HERE', counter)
-            // for await (const i of counter) {
-            //     const contract = await contractList.methods.contracts(i).call();
-            //     setContracts((contacts) => [...contacts, contract]);
-            // }
+            web3.eth.requestAccounts()
+                .then(processRegister)
+                .catch(err => console.log(err));
         }
        load();
     }, [])
@@ -41,17 +52,19 @@ export const ChildPage = () => {
       <div style={{padding: '1rem'}}>
         <h2>Child page</h2>
           <div className={'content'}>
-            {!isLoading && (parents.length > 0
+            {!isLoading
               ? <ul> {parents.map(parent => <li key={parent}>{parent}</li>)} </ul>
               : <div>
-                 <input
-                   style={{width: "calc(100% - 2.5rem)", marginBottom: '1rem'}}
-                   onInput={event => setParentId(event.target.value)}
-                   placeholder={"Add parent ids here"}
-                 />
-                </div>)
-            }
-        <button onClick={onSave}>Set parent</button>
+
+                </div>
+            }<input
+                style={{width: "calc(100% - 2.5rem)", marginBottom: '1rem'}}
+                onInput={event => setParentId(event.target.value)}
+                placeholder={"Add parent ids here"}
+            />
+            <button onClick={onSave}>Set parent</button>
+            <br/>
+            <button style={{marginTop: '1rem'}} onClick={storeGeo.current}>Send geo</button>
       </div>
     </div>
   );
--- src/features/parentApp/index.jsx
+++ src/features/parentApp/index.jsx
@@ -10,24 +10,46 @@ import {Style, Icon} from "ol/style";
 import {transform, fromLonLat} from 'ol/proj.js';
 import {useEffect, useState, useRef} from "react";
 import OSM from 'ol/source/OSM';
+import {CONTACT_ABI, CONTACT_ADDRESS} from "../../contracts/const";
 import {useChildren} from "./assignedChildren/hooks/useChildren";
 
 export const ParentPage = () => {
     const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-    const { children, loadChildren } = useChildren();
+    const { loadChildren } = useChildren();
     const [map, setMap] = useState();
     const [markers, setMarkers] = useState();
     const [kidPoint, setKidPoint] = useState();
-    const [account, setAccount] = useState();
+
+    const [child, setChild] = useState([]);
     const mapElement = useRef();
     const mapRef = useRef();
     mapRef.current = map;
 
-    useEffect(() => {
-        if(account) console.log('GOT PARENT ACCOUNT', account)
-    }, [account])
+    const getChildGeo = async (contract, parentAccount, childId) => {
+        const geo = await contract.methods.getTrack().call({from: parentAccount});
+        const last = geo[geo.length - 1];
+        setChild({id: childId, coords: { latitude: last.latitude, longitude: last.longitude} })
+        console.log("[NEW GEO FETCHED]", { latitude: last.latitude, longitude: last.longitude});
+    }
 
-    useEffect(()=> {
+    const processRegister = async (accounts) => {
+        const [parentAccount] = accounts;
+        const contract = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
+        const child = await contract.methods.getDevice().call({from: parentAccount});
+        console.log("[CHILD]", child);
+        const geo = await contract.methods.getTrack().call({from: parentAccount});
+        setChild({ id: child, geo });
+        await getChildGeo(contract, parentAccount, child)
+        setInterval(() => getChildGeo(contract, parentAccount, child), 5000)
+    }
+
+    useEffect(() => {
+        web3.eth.requestAccounts()
+            .then(processRegister)
+            .catch(err => console.log(err));
+    }, [])
+
+    useEffect(() => {
         if (map){
             const newMarkers = new VectorLayer({
                 source: new VectorSource(),
@@ -43,32 +65,19 @@ export const ParentPage = () => {
         }
     }, [map])
 
-
     useEffect(() => {
-        async function load(){
-            await loadChildren()
-            web3.eth.requestAccounts()
-                .then(accounts => setAccount(accounts?.[0]))
-                .catch(err => console.log('ACC ERR:', err));
-        }
-        load();
-    }, [])
-
-    useEffect(() => {
-        const [child] = children;
         if (!map || !child) return;
         const view = map.getView();
-        view.setCenter(transform([child.coords.longitude, child.coords.latitude], 'EPSG:4326', 'EPSG:3857'));
+        view.setCenter(transform([child?.coords?.longitude, child?.coords?.latitude], 'EPSG:4326', 'EPSG:3857'));
         view.setZoom(15)
-
-        const point = new Feature(new Point(fromLonLat([child.coords.longitude, child.coords.latitude])));
+        const point = new Feature(new Point(fromLonLat([child?.coords?.longitude, child?.coords?.latitude])));
         if (markers){
             const source = markers.getSource();
-            source.removeFeature(kidPoint);
+            // source.removeFeature(kidPoint);
             source.addFeature(point);
             setKidPoint(point);
         }
-    }, [children, map, markers])
+    }, [child, map, markers])
 
     useEffect(() => {
         const initialMap = new Map({
